e6d1b9d9e037110b22d23df76b45b8473a402d0d
Matheus Moscardini <matheus_moscardini@yahoo.com.br>
2017-10-04 18:54:31 -0300

    [ADD] Started angular project. Added form de cadastro

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { MapComponent } from './components/map/map.component';
+import { TableComponent } from './components/table/table.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    MapComponent,
+    TableComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
